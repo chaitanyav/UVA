76327ae89c581114814c9c1514bd325fde140bc3
NagaChaitanya Vellanki <nvellanki@nvidia.com>
2013-12-26 16:59:21 -0800

    Use integers instead of strings for tracking

--- 11860.cpp
+++ 11860.cpp
@@ -12,18 +12,20 @@
 #define sz(c) (int)c.size()
 
 int main(int argc, char *argv[]) {
-  int numDocuments, document = 0;
+  int numDocuments, document = 0, numUniqueWords, wordsCounter;
   std::string input;
   std::stringstream ss;
-  std::set<std::string> uniqueWords;
-  std::vector<std::string> words;
+  std::map<std::string, int> indexByWord;
+  std::map<int, int> indexByWordNumber;
 
   scanf("%d", &numDocuments);
   std::getline(std::cin, input);
 
   while (numDocuments > 0) {
-    uniqueWords.clear();
-    words.clear();
+    indexByWord.clear();
+    indexByWordNumber.clear();
+    wordsCounter = 0;
+    numUniqueWords = 0;
 
     while (std::getline(std::cin, input), input.compare("END") != 0) {
       ss.clear();
@@ -35,34 +37,44 @@ int main(int argc, char *argv[]) {
           word += *it;
         }
         else if (word != "" && !isalpha(*it)){
-          uniqueWords.insert(word);
-          words.push_back(word);
+          if(indexByWord.find(word) == indexByWord.end()) {
+            indexByWord[word] = ++numUniqueWords;
+            ++wordsCounter;
+            indexByWordNumber[wordsCounter] = numUniqueWords;
+          } else {
+            ++wordsCounter;
+            indexByWordNumber[wordsCounter] = indexByWord[word];
+          }
           word = "";
         }
       }
       if (word != ""){
-        uniqueWords.insert(word);
-        words.push_back(word);
+        if(indexByWord.find(word) == indexByWord.end()) {
+          indexByWord[word] = ++numUniqueWords;
+          ++wordsCounter;
+          indexByWordNumber[wordsCounter] = numUniqueWords;
+        } else {
+          ++wordsCounter;
+          indexByWordNumber[wordsCounter] = indexByWord[word];
+        }
       }
     }
 
     std::pair<int, int> minRange(1, 999999);
-    for (int i = 0; i < sz(words); i++) {
+    for (int i = 1; i <= wordsCounter; i++) {
       int index1 = i;
       int index2 = 0;
-      std::set<std::string> seenWords;
-      for (int j = i; (j < sz(words)) && (j - i) < sz(uniqueWords); j++) {
-        if (seenWords.find(words[j]) == seenWords.end()) {
-          seenWords.insert(words[j]);
+      std::set<int> seenIndices;
+      for (int j = i; (j <= wordsCounter) && (j - i) <= numUniqueWords; j++) {
+        if (seenIndices.find(indexByWordNumber[j]) == seenIndices.end()) {
+          seenIndices.insert(indexByWordNumber[j]);
           index2 = j;
         } else {
           break;
         }
       }
 
-      if (sz(seenWords) == sz(uniqueWords)) {
-        index1++;
-        index2++;
+      if (sz(seenIndices) == numUniqueWords) {
         if((index2 - index1) < (minRange.second - minRange.first)) {
           minRange.first = index1;
           minRange.second = index2;
