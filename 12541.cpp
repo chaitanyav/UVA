90d2d377d23fab71c2b926d748a5c7b548bc6df4
NagaChaitanya Vellanki <nvellanki@nvidia.com>
2014-05-05 17:31:05 -0700

    Add 12541.cpp (Birthdates solved)

--- 12541.cpp
+++ 12541.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
-#include <climits>
+#include <vector>
+#include <algorithm>
+
+typedef struct Person {
+ int month;
+ int date;
+ int year;
+ std::string name;
+}Person;
+
+bool custom_compare(const Person& person1, const Person& person2) {
+  bool result = false;
+  if(person1.year < person2.year) {
+    result = true;
+  } else if(person1.year == person2.year) {
+    if(person1.month < person2.month) {
+      result = true;
+    } else if(person1.month == person2.month) {
+      if(person1.date < person2.date) {
+        result = true;
+      } else {
+        result = false;
+      }
+    }
+  }
+
+  return result;
+}
 
 int main(int argc, char *argv[]) {
-  int num_inputs, n, date, month, year, youngest_age = INT_MAX, oldest_age = INT_MIN, age;
+  int num_inputs, n, date, month, year;
+  std::vector<Person> persons;
   std::cin >> num_inputs;
   n = num_inputs;
 
@@ -10,19 +38,18 @@ int main(int argc, char *argv[]) {
 
   while(n > 0) {
     std::cin >> name >> date >> month >> year;
-    age = date + month * 100 + year * 10000;
-    std::cout << age << " " << youngest_age << " " << oldest_age << std::endl;
-    if(age < youngest_age) {
-      youngest_age = age;
-      youngest = name;
-    }
-    if(age > oldest_age) {
-      oldest_age = age;
-      oldest = name;
-    }
+    Person person;
+    person.name = name;
+    person.date = date;
+    person.month = month;
+    person.year = year;
+    persons.push_back(person);
     n--;
   }
 
-  std::cout << oldest << std::endl << youngest << std::endl;
+  std::sort(persons.begin(), persons.end(), custom_compare);
+  int num_persons = (int) persons.size();
+  std::cout << persons[num_persons - 1].name << std::endl << persons[0].name << std::endl;
+
   return 0;
 }
