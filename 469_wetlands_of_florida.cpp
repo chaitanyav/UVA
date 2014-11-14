845d4929de7f98f7507d22e52171cd2fac80ed64
NagaChaitanya Vellanki <me@chaitanyavellanki.com>
2014-11-13 20:22:03 -0800

    469 Wetlands of Florida (Solved)

--- 469_wetlands_of_florida.cpp
+++ 469_wetlands_of_florida.cpp
@@ -14,23 +14,43 @@ static std::vector<std::vector<bool> > visited;
 static int area = 0;
 
 void floodFill(int i, int j, int color) {
-  std::cout << "For " << i << " , " << j << "with color " << color << "visited is " << (visited[i])[j] << std::endl;
-  std::cout << (i > (int) adjList.size()) << std::endl;
-  std::cout << (j > (int) adjList.size()) << std::endl;
-  std::cout << (i < 0) << std::endl;
-  std::cout << (j < 0) << std::endl;
-  std::cout <<((i < 0) || (j < 0) || (i > (int) adjList.size()) || (j > (int) adjList.size()) || ((visited[i])[j]) || (color != 2))  << std::endl;
   if((i < 0) || (j < 0) || (i > (int) adjList.size()) || (j > (int) adjList.size()) || ((visited[i])[j]) || (color != 2)) {
     return;
   }
 
-  std::cout << i << " , " << j <<  " color is " << color << std::endl;
-  (visited[i])[j] = true;
+  ((visited[i])[j]) = true;
   area++;
-  floodFill(i - 1, j , ((adjList[i - 1])[j]).second);
-  floodFill(i + 1, j, ((adjList[i + 1])[j]).second);
-  floodFill(i, j - 1, ((adjList[i])[j - 1]).second);
-  floodFill(i, j + 1, ((adjList[i])[j + 1]).second);
+  if(((i - 1) >= 0) && (j >= 0) && ((i - 1) < (int) adjList.size()) && (j < (int) adjList.size())) {
+      floodFill(i - 1, j, ((adjList[i - 1])[j]).second);
+  }
+
+  if(((i + 1) >= 0) && (j >= 0) && ((i + 1) < (int) adjList.size()) && (j < (int) adjList.size())) {
+    floodFill(i + 1, j, ((adjList[i + 1])[j]).second);
+  }
+
+  if((i >= 0) && ((j - 1) >= 0) && (i < (int) adjList.size()) && ((j - 1) < (int) adjList.size())) {
+    floodFill(i, j - 1, ((adjList[i])[j - 1]).second);
+  }
+
+  if((i >= 0) && ((j + 1) >= 0) && (i < (int) adjList.size()) && ((j + 1) < (int) adjList.size())) {
+    floodFill(i, j + 1, ((adjList[i])[j + 1]).second);
+  }
+
+  if(((i - 1) >= 0) && ((j - 1) >= 0) && ((i - 1) < (int) adjList.size()) && ((j - 1) < (int) adjList.size())) {
+    floodFill(i - 1, j - 1, ((adjList[i - 1])[j - 1]).second);
+  }
+
+  if(((i - 1) >= 0) && ((j + 1) >= 0) && ((i - 1) < (int) adjList.size()) && ((j + 1) < (int) adjList.size())) {
+    floodFill(i - 1, j + 1, ((adjList[i - 1])[j + 1]).second);
+  }
+
+  if(((i + 1) >= 0) && ((j - 1) >= 0) && ((i + 1) < (int) adjList.size()) && ((j - 1) < (int) adjList.size())) {
+    floodFill(i + 1, j - 1, ((adjList[i + 1])[j - 1]).second);
+  }
+
+  if(((i + 1) >= 0) && ((j + 1) >= 0) && ((i + 1) < (int) adjList.size()) && ((j + 1) < (int) adjList.size())) {
+    floodFill(i + 1, j + 1, ((adjList[i + 1])[j + 1]).second);
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -64,7 +84,8 @@ int main(int argc, char *argv[]) {
         sbuf >> i >> j;
         visited.clear();
         for(int row = 0; row < (int) adjList.size(); row++) {
-          visited.push_back(std::vector<bool>(false, adjList.size()));
+          std::vector<bool> temp(adjList.size(), false);
+          visited.push_back(temp);
         }
         floodFill(i - 1, j - 1, 2);
         std::cout << area << std::endl;
@@ -73,6 +94,15 @@ int main(int argc, char *argv[]) {
     } else {
       neighbors.clear();
       adjList.clear();
+
+      numTestCases--;
+      if(!numTestCases) {
+        break;
+      }
+      else {
+        std::cout << std::endl;
+      }
+
     }
   }
   return 0;
