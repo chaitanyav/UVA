14ae688775a99667c9bafbeb9a79082f1519c1db
NagaChaitanya Vellanki <nvellanki@nvidia.com>
2014-11-14 16:21:37 -0800

    Improve 657 the die is cast

--- 657_the_die_is_cast.cpp
+++ 657_the_die_is_cast.cpp
@@ -10,41 +10,73 @@
 // 2 --- *
 // 3 --- X
 
+void floodFill3(int i, int j, int color);
+void floodFill(int i, int j, int color);
 typedef std::pair<int, int> VertexWeight;
 typedef std::vector<VertexWeight> Neighbors;
 typedef std::vector<Neighbors> AdjList;
 std::vector<std::vector<bool> > visited;
-std::set<int> areas;
+std::vector<int> areas;
+int area = 0;
 
 Neighbors neighbors;
 AdjList adjList;
 
-int floodFill(int i, int j, int color) {
-  int area = 0;
+void floodFill3(int i, int j, int color) {
   if((i < 0) || (j < 0) || (i > (int) adjList.size()) || (j > (int) (adjList[i]).size()) || ((visited[i])[j]) || (color != 3)) {
-    return 0;
+      if((color == 2) && !((visited[i])[j])) {
+        //std::cout << "Calling flood fill " << i << " , " << j << std::endl;
+        floodFill(i, j, 2);
+      } else {
+        return;
+      }
   }
 
   ((visited[i])[j]) = true;
-  area = 1;
-  std::cout << i << " , " << j << std::endl;
   if(((i - 1) >= 0) && (j >= 0) && ((i - 1) < (int) adjList.size()) && (j < (int) (adjList[i]).size())) {
-    area += floodFill(i - 1, j, ((adjList[i - 1])[j]).second);
+    floodFill3(i - 1, j, ((adjList[i - 1])[j]).second);
   }
 
   if(((i + 1) >= 0) && (j >= 0) && ((i + 1) < (int) adjList.size()) && (j < (int) (adjList[i]).size())) {
-    area += floodFill(i + 1, j, ((adjList[i + 1])[j]).second);
+    floodFill3(i + 1, j, ((adjList[i + 1])[j]).second);
   }
 
   if((i >= 0) && ((j - 1) >= 0) && (i < (int) adjList.size()) && ((j - 1) < (int) (adjList[i]).size())) {
-    area += floodFill(i, j - 1, ((adjList[i])[j - 1]).second);
+    floodFill3(i, j - 1, ((adjList[i])[j - 1]).second);
   }
 
   if((i >= 0) && ((j + 1) >= 0) && (i < (int) adjList.size()) && ((j + 1) < (int) (adjList[i]).size())) {
-    area += floodFill(i, j + 1, ((adjList[i])[j + 1]).second);
+    floodFill3(i, j + 1, ((adjList[i])[j + 1]).second);
+  }
+}
+
+void floodFill(int i, int j, int color) {
+  if((i < 0) || (j < 0) || (i > (int) adjList.size()) || (j > (int) (adjList[i]).size()) || ((visited[i])[j]) || (color != 2)) {
+    if(color == 3 && !((visited[i])[j])) {
+      area++;
+      //std::cout << "calling flood fillx " << i << " , " << j << std::endl;
+      floodFill3(i, j, 3);
+    } else {
+      return;
+    }
   }
 
-  return area;
+  ((visited[i])[j]) = true;
+  if(((i - 1) >= 0) && (j >= 0) && ((i - 1) < (int) adjList.size()) && (j < (int) (adjList[i]).size())) {
+    floodFill(i - 1, j, ((adjList[i - 1])[j]).second);
+  }
+
+  if(((i + 1) >= 0) && (j >= 0) && ((i + 1) < (int) adjList.size()) && (j < (int) (adjList[i]).size())) {
+    floodFill(i + 1, j, ((adjList[i + 1])[j]).second);
+  }
+
+  if((i >= 0) && ((j - 1) >= 0) && (i < (int) adjList.size()) && ((j - 1) < (int) (adjList[i]).size())) {
+    floodFill(i, j - 1, ((adjList[i])[j - 1]).second);
+  }
+
+  if((i >= 0) && ((j + 1) >= 0) && (i < (int) adjList.size()) && ((j + 1) < (int) (adjList[i]).size())) {
+    floodFill(i, j + 1, ((adjList[i])[j + 1]).second);
+  }
 }
 
 
@@ -67,7 +99,7 @@ int main(int argc, char *argv[]) {
         vertexWeight.first = pos;
         char temp = input.at(pos);
         if(temp == '.') {
-            vertexWeight.second = 1;
+          vertexWeight.second = 1;
         } else if(temp == '*') {
           vertexWeight.second = 2;
         } else {
@@ -83,23 +115,25 @@ int main(int argc, char *argv[]) {
         }
         for(int i = 0; i < height; ++i) {
           for(int j = 0; j < width; ++j) {
-            if((visited[i])[j] == false) {
-              int area = floodFill(i, j, ((adjList[i])[j]).second);
-              if(((adjList[i])[j]).second == 3) {
-                areas.insert(area);
-              }
+            if((visited[i])[j] == false && ((adjList[i])[j]).second != 1) {
+              area = 0;
+              floodFill(i, j, ((adjList[i])[j]).second);
+              if(area)
+              areas.push_back(area);
             }
           }
         }
 
         std::cout << "Throw " << ++attempt << std::endl;
+        std::sort(areas.begin(), areas.end());
         int numAreas = (int)areas.size() - 1;
-        for(std::set<int>::iterator it = areas.begin(); it != areas.end(); ++it) {
+        for(std::vector<int>::iterator it = areas.begin(); it != areas.end(); ++it) {
           std::cout << *it;
           if(numAreas--) {
             std::cout << " ";
           }
         }
+        std::cout << std::endl;
         std::cout << std::endl;
       }
     } else {
